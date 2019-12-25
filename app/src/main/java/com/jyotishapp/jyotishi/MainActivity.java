9dbb96e7fe8707144b5b95f9f09cb9c958a0ce50
Anubhav Mittal <anubhav11march@gmail.com>
2019-12-25 13:46:20 +0530

    :truck: Configured Google Login

--- app/src/main/java/com/jyotishapp/jyotishi/MainActivity.java
+++ app/src/main/java/com/jyotishapp/jyotishi/MainActivity.java
@@ -1,13 +1,29 @@
 package com.jyotishapp.jyotishi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.nightonke.boommenu.BoomButtons.HamButton;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomMenuButton;
@@ -16,14 +32,32 @@ import com.nightonke.boommenu.Util;
 
 public class MainActivity extends AppCompatActivity {
     BoomMenuButton bmb;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
+    private int RC_SIGNIN = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        //animation buttons
         bmb = (BoomMenuButton) findViewById(R.id.bmbHam);
         bmb.setButtonEnum(ButtonEnum.Ham);
         bmb.setBottomHamButtonTopMargin(Util.dp2px(70));
+
+        //google login
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("69525632215-182s7a2e18dvi6rqmpnveci143t3olv3.apps.googleusercontent.com")
+                .requestEmail()
+                .requestProfile()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+
         addBuilder();
     }
 
@@ -36,6 +70,12 @@ public class MainActivity extends AppCompatActivity {
                 .highlightedColorRes(R.color.bittonHigh0)
                 .textPadding(new Rect(0, 30, 0, 0))
                 .containsSubText(false)
+                .listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+                        googleSignIn();
+                    }
+                })
                 .imagePadding(new Rect(10, 10, 10, 1));
         HamButton.Builder builder1 = new HamButton.Builder()
                 .normalImageRes(R.drawable.phone)
@@ -74,5 +114,48 @@ public class MainActivity extends AppCompatActivity {
             bmb.addBuilder(builder1);
             bmb.addBuilder(builder2);
 
+    }
+
+    protected void googleSignIn(){
+        if(firebaseUser != null)
+            Toast.makeText(MainActivity.this, "Signed in as: " + firebaseUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+        Intent googleSignInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent, RC_SIGNIN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(mAuth.getCurrentUser() != null)
+            return;
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGNIN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+                Toast.makeText(MainActivity.this, "Signed in as: " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+            }catch (ApiException e){
+                Toast.makeText(MainActivity.this, "Couldn't sign in ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.v("AAAA", "Success");
+                            startActivity(new Intent(MainActivity.this, MainScreen.class));
+                            Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "An error occured", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
