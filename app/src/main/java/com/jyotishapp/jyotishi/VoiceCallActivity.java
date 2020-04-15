57ef78100717a41e4351ccc6e3cdf78d37d8bc19
Anubhav Mittal <anubhav11march@gmail.com>
2020-04-15 13:53:52 +0530

    :zap: Audio features updated

--- app/src/main/java/com/jyotishapp/jyotishi/VoiceCallActivity.java
+++ app/src/main/java/com/jyotishapp/jyotishi/VoiceCallActivity.java
@@ -1,21 +1,49 @@
 package com.jyotishapp.jyotishi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.animation.LayoutTransition;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Locale;
 
 import io.agora.rtc.Constants;
 import io.agora.rtc.IRtcEngineEventHandler;
 import io.agora.rtc.RtcEngine;
 
 public class VoiceCallActivity extends AppCompatActivity {
+
+    boolean muteOn = false, speakerOn = false, holdOn = false;
+    private TextView timer;
+    private ImageView mute, speaker, hold;
+    LinearLayout endCall, muteContainer, holdConatiner, speakerContainer;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference mRef;
     private final static int PERMISSION_REQ_ID_RECORD_AUDIO = 22;
     private RtcEngine rtcEngine;
     private final IRtcEngineEventHandler rtcEngineEventHandler = new IRtcEngineEventHandler() {
@@ -58,9 +86,126 @@ public class VoiceCallActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_call);
         getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        mRef = database.getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+        mRef.child("Calling").setValue(true);
+        mRef.child("Engaged").setValue(false);
+
+        timer = (TextView) findViewById(R.id.timer);
+        mute = (ImageView) findViewById(R.id.mute);
+        speaker = (ImageView) findViewById(R.id.speaker);
+        hold = (ImageView) findViewById(R.id.hold);
+        endCall = (LinearLayout) findViewById(R.id.end_call);
+        muteContainer = (LinearLayout) findViewById(R.id.mute_container);
+        speakerContainer = (LinearLayout) findViewById(R.id.speaker_container);
+        holdConatiner = (LinearLayout) findViewById(R.id.hold_container);
+        seconds =0;
+
+        mute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(muteOn) {
+                    Toast.makeText(VoiceCallActivity.this, "Mic on", Toast.LENGTH_LONG).show();
+                    muteOn = false;
+                    muteContainer.setBackground(getDrawable(R.color.white));
+                    mute.setColorFilter(Color.argb(255, 0, 0, 0));
+                } else {
+                    Toast.makeText(VoiceCallActivity.this, "Mic off", Toast.LENGTH_LONG).show();
+                    muteOn = true;
+                    muteContainer.setBackground(getDrawable(R.color.black));
+                    mute.setColorFilter(Color.argb(255, 255, 255, 255));
+                }
+            }
+        });
+
+        hold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(holdOn) {
+                    Toast.makeText(VoiceCallActivity.this, "Call resumed", Toast.LENGTH_LONG).show();
+                    holdOn = false;
+                    holdConatiner.setBackground(getDrawable(R.color.white));
+                    hold.setColorFilter(Color.argb(255, 0, 0, 0));
+                } else {
+                    Toast.makeText(VoiceCallActivity.this, "Call put on hold", Toast.LENGTH_LONG).show();
+                    holdOn = true;
+                    holdConatiner.setBackground(getDrawable(R.color.black));
+                    hold.setColorFilter(Color.argb(255, 255, 255, 255));
+                }
+            }
+        });
+
+        speaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(speakerOn) {
+                    Toast.makeText(VoiceCallActivity.this, "Hands Free On", Toast.LENGTH_LONG).show();
+                    speakerOn = false;
+                    speakerContainer.setBackground(getDrawable(R.color.white));
+                    speaker.setColorFilter(Color.argb(255, 0, 0, 0));
+                } else {
+                    Toast.makeText(VoiceCallActivity.this, "Speaker on", Toast.LENGTH_LONG).show();
+                    speakerOn = true;
+                    speakerContainer.setBackground(getDrawable(R.color.black));
+                    speaker.setColorFilter(Color.argb(255, 255, 255, 255));
+                }
+            }
+        });
+
+        endCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(VoiceCallActivity.this, "Call ended", Toast.LENGTH_LONG).show();
+                onStop();
+                finish();
+            }
+        });
+
+        mRef.child("Engaged").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue().toString().equals(true+"")){
+                    changeUiToCall();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         if(checkSelfPermission(PERMISSION_REQ_ID_RECORD_AUDIO))
             initializeAgoraEngineAndJoinChannel();
+    }
+
+    private void changeUiToCall(){
+        mute.setColorFilter(Color.argb(255, 0, 0, 0));
+        speaker.setColorFilter(Color.argb(255, 0, 0, 0));
+        hold.setColorFilter(Color.argb(255, 0, 0, 0));
+        timerStart();
+    }
+
+    int seconds;
+
+    private void timerStart(){
+//        ViewGroup parent = (ViewGroup) this.timer.getParent();
+//        parent.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+        timer.setText("00:00");
+        mute.setClickable(true);
+        speaker.setClickable(true);
+        hold.setClickable(true);
+        final Handler timerHandler = new Handler();
+        timerHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                String timerText = String.format(Locale.getDefault(), "%02d:%02d", seconds/60, seconds%60);
+                timer.setText(timerText);
+                seconds++;
+                timerHandler.postDelayed(this, 1000);
+            }
+        });
     }
 
     private void initializeAgoraEngineAndJoinChannel(){
@@ -107,5 +252,11 @@ public class VoiceCallActivity extends AppCompatActivity {
         leaveChannel();
         RtcEngine.destroy();
         rtcEngine = null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
