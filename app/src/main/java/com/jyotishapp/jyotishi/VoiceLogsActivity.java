2fedf48c2cdacc4f2b0f3ee06560e6caf041fcf8
Anubhav Mittal <anubhav11march@gmail.com>
2020-04-23 18:52:39 +0530

    :zap: Added Fuctionalities to Call logs

--- app/src/main/java/com/jyotishapp/jyotishi/VoiceLogsActivity.java
+++ app/src/main/java/com/jyotishapp/jyotishi/VoiceLogsActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -22,6 +26,7 @@ import com.google.firebase.database.Query;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class VoiceLogsActivity extends AppCompatActivity {
 
@@ -29,6 +34,8 @@ public class VoiceLogsActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference mRef;
+    LinearLayout faceCard, expandableView;
+    ImageView userIcon, viewProfile, viewChat, viewVoiceCall, viewVideoCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,12 +69,89 @@ public class VoiceLogsActivity extends AppCompatActivity {
 
         final FirebaseRecyclerAdapter FBRA = new FirebaseRecyclerAdapter<VoiceCall, VoiceCallHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull VoiceCallHolder holder, int position, @NonNull VoiceCall model) {
+            protected void onBindViewHolder(@NonNull final VoiceCallHolder holder, int position, @NonNull VoiceCall model) {
                 holder.setName(model.getContactUserName());
                 holder.setContact(model.getContactInfo());
                 holder.setLogIcon(model.getType());
                 holder.setTimeDuration(model.getTimestamp(), model.getDuration());
-            }
+                faceCard = (LinearLayout) holder.mView.findViewById(R.id.face_card);
+                faceCard.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        expandableView = (LinearLayout) holder.mView.findViewById(R.id.expanded_view);
+                        if(expandableView.getVisibility() == View.GONE) {
+                            expandableView.animate()
+                                    .translationY(0)
+                                    .setDuration(1000)
+                                    .alpha(1.0f)
+                                    .setListener(new AnimatorListenerAdapter() {
+                                        @Override
+                                        public void onAnimationStart(Animator animation) {
+                                            super.onAnimationStart(animation);
+                                            expandableView.setVisibility(View.VISIBLE);
+                                        }
+                                    });
+                        }
+                        else {
+                            expandableView.setVisibility(View.GONE);
+//                            expandableView.animate()
+//                                    .translationY(0)
+//                                    .setDuration(1000)
+//                                    .setListener(new AnimatorListenerAdapter() {
+//                                        @Override
+//                                        public void onAnimationEnd(Animator animation) {
+//                                            super.onAnimationEnd(animation);
+//
+//                                        }
+//                                    });
+
+                        }
+                    }
+                });
+                userIcon = (ImageView) holder.mView.findViewById(R.id.user_ico);
+                Random rand = new Random();
+                int x = rand.nextInt(5);
+                switch (x){
+                    case 1: userIcon.setColorFilter(Color.argb(255, 0, 128, 255));
+                            break;
+                    case 0: userIcon.setColorFilter(Color.argb(255, 246, 145, 65));
+                        break;
+                    case 2: userIcon.setColorFilter(Color.argb(255, 255, 97, 187));
+                        break;
+                    case 3: userIcon.setColorFilter(Color.argb(255, 173, 92, 247));
+                        break;
+                    case 4: userIcon.setColorFilter(Color.argb(255, 90, 179, 113));
+                        break;
+                }
+                viewChat = (ImageView) holder.mView.findViewById(R.id.view_chat);
+                viewProfile = (ImageView) holder.mView.findViewById(R.id.view_profile);
+                viewVoiceCall = (ImageView) holder.mView.findViewById(R.id.view_voice_call);
+                viewVideoCall = (ImageView) holder.mView.findViewById(R.id.view_video_call);
+                viewChat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(VoiceLogsActivity.this, ChatActivity.class));
+                    }
+                });
+                viewProfile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(VoiceLogsActivity.this, JyotishProfilesActivity.class));
+                    }
+                });
+                viewVideoCall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(VoiceLogsActivity.this, VidCallActivity.class));
+                    }
+                });
+                viewVoiceCall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(VoiceLogsActivity.this, VoiceCallActivity.class));
+                    }
+                });
+             }
 
             @NonNull
             @Override
@@ -75,6 +159,7 @@ public class VoiceLogsActivity extends AppCompatActivity {
                 View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.call_log_card, parent, false);
                 return new VoiceCallHolder(itemView);
             }
+
         };
         FBRA.startListening();
         callLogs.setAdapter(FBRA);
